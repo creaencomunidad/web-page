d9835eddd5010dd8915c0969c235347585001468
orozcoh <dev.orozcoh@gmail.com>
2024-11-07 18:36:02 -0500

    add site in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.creaencomunidad.com",
   integrations: [react(), tailwind()],
 });
